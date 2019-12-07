c0ae1bc7d44886d5e76399208150328f3f35c78e
premt <premtim.farizi@gmail.com>
2019-12-07 05:06:24 +0100

    Finished Jokes App with jokes from SpringFrameworkGuru

--- src/main/java/com/premt/joke/jokeapp/JokeappApplication.java
+++ src/main/java/com/premt/joke/jokeapp/JokeappApplication.java
@@ -3,7 +3,7 @@ package com.premt.joke.jokeapp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.premt.joke.jokeapp","com.premt.joke"})
 public class JokeappApplication {
 
     public static void main(String[] args) {
---
+++ src/main/java/com/premt/joke/jokeapp/service/JokeService.java
@@ -0,0 +1,5 @@
+package com.premt.joke.jokeapp.service;
+
+public interface JokeService {
+    String getJoke();
+}
